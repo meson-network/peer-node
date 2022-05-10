fd5c7e6188a2dfc52cd533aebadc1a0b45056a72
leo <leo@leos-MacBook-Pro.local>
2022-05-10 13:39:27 +0800

    update

--- cmd/default_/plugin/sqlite.go
+++ cmd/default_/plugin/sqlite.go
@@ -12,7 +12,7 @@ func initSqlite() error {
 
 	sqlite_path, sqlite_path_err := storage_mgr.CheckPath("db", "peer.db")
 	if sqlite_path_err != nil {
-		return errors.New("db file not found:" + sqlite_path_err.Error() + " please check storage folder path , if still not working please re-download your  program and restart")
+		return errors.New("db file not found," + sqlite_path_err.Error() + "\n please check storage folder path , if still not working please re-download your  program and restart")
 	}
 
 	return sqlite_plugin.Init(sqlite_plugin.Config{
